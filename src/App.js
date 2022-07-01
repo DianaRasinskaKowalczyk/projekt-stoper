21e187be858ed34dc73e2ad3dab3bbd9aeda126e
Diana Rasińska-Kowalczyk <diana.rasinska@gmail.com>
2022-07-01 13:43:43 +0200

    Add stoper js

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import Button from './components/Button/Button';
+import React, {useEffect, useState} from "react";
+import Timer from './components/Timer/Timer';
+
+
+const App = () => {
+
+  const [time, setTime] = useState('');
+  const [start, setStart] = useState(false);
+
+
+  useEffect(() => {
+    let interval = null;
+
+    if(start) {
+      interval = setInterval(() => {
+        setTime(prevTime => prevTime + 10)
+      }, 10)
+    } else {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval)
+  }, [start]);
+  
+
+  return(
+    <div>
+      <Timer time={time} />
+      <Button action={() => setStart(true)}>START</Button>
+      <Button action={() => setStart(false)}>STOP</Button>
+      <Button action={() => setTime(0)}>RESET</Button>
+    </div>
+  )
+
+};
+
+export default App;
